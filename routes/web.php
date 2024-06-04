bd066266376714e0f7a69a9c77e6b11b6566a3af
ikram724 <ikrammir714@gmail.com>
2024-06-04 17:10:45 +0500

    ..

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,10 @@ Route::middleware([RoleMiddleware::class . ':admin'])->group(function () {
     Route::resource('students', StudentController::class);
     Route::resource('users', UserController::class);
 });
-Route::middleware([RoleMiddleware::class . ':student'])->group(function () {
-    Route::get('students', [StudentController::class, 'index'])->name('students.index');
-    Route::get('students/create', [StudentController::class, 'create'])->name('students.create');
-    Route::get('students/{student}', [StudentController::class, 'show'])->name('students.show');
-    Route::post('students', [StudentController::class, 'store'])->name('students.store');
-    Route::get('students/{student}/edit', [StudentController::class, 'edit'])->name('students.edit');
-    Route::put('students/{student}', [StudentController::class, 'update'])->name('students.update');
-});
+
+Route::get('students', [StudentController::class, 'index'])->name('students.index');
+Route::get('students/create', [StudentController::class, 'create'])->name('students.create');
+Route::get('students/{student}', [StudentController::class, 'show'])->name('students.show');
+Route::post('students', [StudentController::class, 'store'])->name('students.store');
+Route::get('students/{student}/edit', [StudentController::class, 'edit'])->name('students.edit');
+Route::put('students/{student}', [StudentController::class, 'update'])->name('students.update');
